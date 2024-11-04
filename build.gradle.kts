1ef71c58d25dace62d449b8d3235e660ff08f43a
MoGreene <13blueboy13@daum.net>
2024-11-04 22:47:27 +0900

    init: application setting

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,11 +27,12 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    runtimeOnly("com.h2database:h2")
     runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    testImplementation("com.h2database:h2")
 
     testImplementation("io.kotest:kotest-assertions-core-jvm:5.6.2")
     testImplementation("io.kotest:kotest-runner-junit5-jvm:5.6.2")
